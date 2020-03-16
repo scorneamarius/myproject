db5d8c36ea984e84fe9508e51713ff3c152f156e
fredi <scorneaalfredmarius@yahoo.com>
2020-03-16 23:03:05 +0200

    firebase push and pull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,13 @@ import {AngularFireDatabase} from 'angularfire2/database'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public courses :any[]
-  constructor(db:AngularFireDatabase)
+  public peopleInfo :any[];
+  /*public db:AngularFireDatabase;*/
+  constructor(public db:AngularFireDatabase)
   {
-    db.list('courses').valueChanges().subscribe(courses => {  this.courses = courses; console.log(this.courses); });
+   /* db.list('people').valueChanges().subscribe(data => {  this.peopleInfo = data; console.log(this.peopleInfo); });*/
+  }
+  faceva (){
+    this.db.list('people/people3/username').push("randomName");
   }
 }
