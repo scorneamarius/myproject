838fb15606fc3f3d53ec827eb37e340c51e412cf
RazailaAlexandra <razaila.alexandra@yahoo.com>
2020-05-06 23:08:24 +0300

    very small correction

--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { map } from 'rxjs/operators';
-import {User} from '../classes/users'
-import {Observable} from 'rxjs'
-import {filter} from 'rxjs/operators'
+import {User} from '../classes/users';
+import {Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 import { timingSafeEqual } from 'crypto';
 import { element } from 'protractor';
 import { FindValueSubscriber } from 'rxjs/internal/operators/find';
